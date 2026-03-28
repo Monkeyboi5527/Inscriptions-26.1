520671b5ac3ea472ed15d73e85cc2cac40bac975
MonkeyBoi <monkeylaguri308@gmail.com>
2026-03-27 23:09:19 -0400

    Mod Setup

---
+++ src/main/java/net/monkeyskl/inscriptions/InscriptionsClient.java
@@ -0,0 +1,14 @@
+package net.monkeyskl.inscriptions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InscriptionsClient implements ClientModInitializer {
+
+
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
